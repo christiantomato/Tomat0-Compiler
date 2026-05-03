0f85d74d51f14e14019bfb92decff31ca67a569d
christiantomato <christianntamayo@gmail.com>
2026-05-02 22:24:52 -0400

    doxygen comments!

--- src/include/token.h
+++ src/include/token.h
@@ -1,21 +1,28 @@
-/*
-Token Structure
-
-defines the different types of tokens that our lexer will encounter
-these include:
-- numbers (ex. 123)
-- strings (ex. "hello world")
-- keywords (ex. sout, int, if, while)
-- punctuation/symbols (ex. (), >, =, +)
-
-TokenType type: the type of token (integer, symbol, keyword)
-char* value: the actual value the token holds (5, +, sout)
-*/
+/**
+ * @file token.h
+ * @brief Describes all types of tokens that can be parsed. 
+ *
+ * Defines the different types of tokens that our lexer will encounter.
+ * These include:
+ * - numbers (ex. 123)
+ * - strings (ex. "hello world")
+ * - keywords (ex. sout, int, if, while)
+ * - punctuation/symbols (ex. (), >, =, +)
+ */
 
 #ifndef TOKEN_H
 #define TOKEN_H
 
-typedef enum token_type {
+/**
+ * @enum TokenType
+ * @brief Represents all possible token types produced by the lexer. 
+ * 
+ * This enumeration defines the different categories of tokens
+ * recognized in the Tomat0 language, including keywords, identifiers,
+ * literals, operators, and punctuation.
+ */
+
+typedef enum {
     TOKEN_KEYWORD_INT,
     TOKEN_KEYWORD_STRING,
     TOKEN_KEYWORD_SOUT,
@@ -39,25 +46,64 @@ typedef enum token_type {
     TOKEN_BSLASH,
     TOKEN_PLUS,
     TOKEN_HYPHEN,
-    TOKEN_ASTERIK,
+    TOKEN_ASTERISK,
     TOKEN_QUESTION,
     TOKEN_EOF
 } TokenType;
 
-typedef struct token_struct {
-    TokenType type;
-    char* value;
+/**
+ * @struct Token
+ * @brief Encodes necessary information for each token. 
+ */
+
+typedef struct {
+    TokenType type; /**< The type of token it is. */
+    char* value; /**< The actual data of the token.  */
 } Token; 
 
-//for creating tokens
+/**
+ * @brief Creates and initializes a new token. 
+ * 
+ * @param type The token type. 
+ * @param value The token data. 
+ * @return Token* Pointer to the created token. 
+ */
+
 Token* init_token(TokenType type, char* value);
-//returning the enum name for the type of token as a string
+
+/**
+ * @brief Returns the enum name for the token as a string. 
+ * 
+ * @param token Pointer to the token. 
+ * @return char* String corresponding to the enum name. 
+ */
+
 char* token_type_as_str(Token* token);
-//performs a to string of token type and value
+
+/**
+ * @brief ToString for a token. 
+ * 
+ * @param token Generic pointer which should be a token (so we can pass the ToString to other functions).
+ * @return char* String representation of the token. 
+ */
+
 char* token_to_str(void* token);
-//for freeing the memory of a token
+
+/**
+ * @brief Frees the allocated memory for a token. 
+ * 
+ * @param token Pointer to the token. 
+ * @return 0 if successful, 1 otherwise. 
+ */
+
 int free_token(Token* token);
-//wrapper function so we can free tokens stored in a built-in array list
+
+/**
+ * @brief Wrapper for the free token function. 
+ * 
+ * @param token Generic pointer to the token. 
+ */
+
 void free_token_wrapper(void* token);
 
 #endif
