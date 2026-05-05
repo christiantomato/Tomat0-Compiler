c989cc02de5c39e711d0e7824003d612f5e6db7f
christiantomato <christianntamayo@gmail.com>
2026-05-05 12:27:30 -0400

    main flow is better

--- src/main.c
+++ src/main.c
@@ -5,10 +5,10 @@
 int main(int argc, char *argv[]) {
 
     /*
-    LEXICAL ANALYSIS
-    */
+     * LEXICAL ANALYSIS
+     */
 
-    //read tomat0 code from file
+    //read tomat0 source code from file
     char* file_contents = read_file("main.tmt");
 
     //initalize the lexer with contents, tokenize and store all to a list
@@ -24,29 +24,34 @@ int main(int argc, char *argv[]) {
     free_lexer(my_lexer);
 
     /*
-    SYNTACTIC ANALYSIS
-    */
+     * SYNTACTIC ANALYSIS
+     */
 
     //intialize the parser and symbol table
     Parser* my_parser = init_parser(tokens_list);
     SymbolTable* my_symbol_table = init_symbol_table();
 
-    //parse everything and get root
+    //parse everything and return the root node
     ASTNode* ast_root = parser_parse(my_parser, my_symbol_table);
 
-    //write ast representation from root now
+    //we can free the tokens and parser now
+    free_list(tokens_list, free_token_wrapper);
+    free_parser(my_parser);
+
+    //write ast representation from root node now
     FILE* ast_file = fopen("output/ast_output.txt", "w");
     print_ast(ast_file, ast_root, 0);
     fclose(ast_file);
+
+    /*
+     * CODE GENERATION
+     */
 
     //generate the assembly code
     FILE* assembly_file = fopen("output/generated_asm.s", "w");
     generate_assembly(assembly_file, ast_root, my_symbol_table);
     //close the file
     fclose(assembly_file);
-
-    //free rest of memory which is being used by parser
-    //free_parser(my_parser);
 
     //make an executable
     system("gcc output/generated_asm.s -o tomat0executable");
