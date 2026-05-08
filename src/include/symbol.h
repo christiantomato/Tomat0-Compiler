31932035feb51f41b70642183f43eb55fe5fb01e
christiantomato <christianntamayo@gmail.com>
2026-05-08 17:58:12 -0400

    add special data for symbols

--- src/include/symbol.h
+++ src/include/symbol.h
@@ -6,14 +6,18 @@
 #ifndef SYMBOL_H
 #define SYMBOL_H
 
+#include "types.h"
+#include "array_list.h"
+#include <stdbool.h>
+
 /**
  * @enum SymbolType
  * @brief Enum for the different types of symbols we can encounter. 
  * 
  * These include: 
- * - variable names
- * - function names
- * - parameter names
+ * - variables
+ * - functions
+ * - parameters
  */
 
 typedef enum {
@@ -23,20 +27,55 @@ typedef enum {
  } SymbolKind;
 
 /**
+ * @struct VariableSymbol
+ * @brief Encodes necessary data needed for a variable.
+ */
+
+typedef struct {
+    DataType type; /**< The data type. */
+    bool is_static; /**< Is the variable local or static? */
+    int offset; /**< The stack relative offset (not used for static variables). */
+} VariableSymbol;
+
+/**
+ * @struct FunctionSymbol
+ * @brief Encpdes necessary data needed for a function. 
+ */
+
+typedef struct {
+    DataType return_type; /**< The return type. */
+    List* parameters; /**< The list of parameters. */
+} FunctionSymbol;
+
+/**
+ * @struct ParameterSymbol
+ * @brief Encodes necessary data need for a parameter.
+ */
+
+typedef struct {
+    DataType type; /**< The data type. */
+} ParameterSymbol;
+
+/**
+ * @union SymbolData
+ * @brief A union so we can store specific data for each type.
+ */
+
+typedef union {
+    VariableSymbol var_sym;
+    FunctionSymbol func_sym;
+    ParameterSymbol param_sym;
+} SymbolData;
+
+/**
  * @struct Symbol
- * @brief Defines the data needed to represent a symbol. 
- * 
- * This data is scope relative. 
+ * @brief Defines all symbol types. 
  */
 
 typedef struct {
     char* name; /**< The symbol name. */
     SymbolKind kind; /**< The kind of symbol. */
-    int offset; /**< Offset relative to the frame pointer in its current stack frame. */
-
-    //future? 
-    //int size
-    //DataType data_type;
+    SymbolData data; /** The data for the symbol. */
 } Symbol;
 
 /**
