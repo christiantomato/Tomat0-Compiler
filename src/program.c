2639967f5f6c3b618f7f6f9925e93359628ca05c
christiantomato <christianntamayo@gmail.com>
2026-05-15 15:57:14 -0400

    add strings list

--- src/program.c
+++ src/program.c
@@ -10,12 +10,13 @@
  * Creates and initalizes the program struct. 
  */
 
-Program* init_program(ASTNode* root, List* scopes) {
+Program* init_program(ASTNode* root, List* scopes, List* strings) {
     //allocate space for the program struct
     Program* program = malloc(sizeof(Program));
-    //set root and scopes
+    //set members
     program->root = root;
     program->scopes = scopes;
+    program->strings = strings;
     return program;
 }
 
@@ -28,4 +29,6 @@ void free_program(Program* program) {
     free_node(program->root);
     //free the scopes
     free_list(program->scopes, free_scope_wrapper);
+    //free the strings
+    free_list(program->strings, free);
 }
