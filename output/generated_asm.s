5b55c8e677a974d1e3b8385b777688b24fb68b39
christiantomato <christianntamayo@gmail.com>
2026-05-17 09:26:01 -0400

    nested subroutine test

--- output/generated_asm.s
+++ output/generated_asm.s
@@ -1,6 +1,5 @@
 .data
 fmt_int: .asciz "%d\n"
-str0: .asciz "the answer is:\n"
 
 .text
 .global _main
@@ -9,16 +8,28 @@ _add:
 	//setup stack frame.
 	stp fp, lr, [sp, #-16]!
 	mov fp, sp
-	sub sp, sp, #16
+	sub sp, sp, #32
 
-	//binary operation.
-	add x9, x0, x1
+	//spill param to stack.
+	str x0, [fp, #-8]
 
-	//store variable to stack.
-	str x9, [fp, #-8]
+	//spill param to stack.
+	str x1, [fp, #-16]
 
 	//load variable from stack.
 	ldr x9, [fp, #-8]
+
+	//load variable from stack.
+	ldr x10, [fp, #-16]
+
+	//binary operation.
+	add x11, x9, x10
+
+	//store variable to stack.
+	str x11, [fp, #-24]
+
+	//load variable from stack.
+	ldr x9, [fp, #-24]
 
 	//return into x0.
 	mov x0, x9
@@ -28,23 +39,47 @@ _add:
 	ldp fp, lr, [sp], #16
 	ret
 
-_double:
+_compose:
 	//setup stack frame.
 	stp fp, lr, [sp, #-16]!
 	mov fp, sp
-	sub sp, sp, #0
+	sub sp, sp, #32
+
+	//spill param to stack.
+	str x0, [fp, #-8]
+
+	//spill param to stack.
+	str x1, [fp, #-16]
+
+	//load variable from stack.
+	ldr x9, [fp, #-8]
+
+	//pass parameter.
+	mov x0, x9
+
+	//load variable from stack.
+	ldr x9, [fp, #-16]
+
+	//pass parameter.
+	mov x1, x9
+
+	//call function.
+	bl _add
 
 	//move number to register.
-	mov x9, #2
+	mov x9, #1
 
 	//binary operation.
-	mul x10, x0, x9
+	add x10, x0, x9
 
-	//moving to param reg.
-	mov x0, x10
+	//store variable to stack.
+	str x10, [fp, #-24]
+
+	//load variable from stack.
+	ldr x9, [fp, #-24]
 
 	//return into x0.
-	mov x0, x0
+	mov x0, x9
 
 	//collapse the stack frame.
 	mov sp, fp
@@ -56,14 +91,6 @@ _main:
 	stp fp, lr, [sp, #-16]!
 	mov fp, sp
 	sub sp, sp, #0
-
-	//load string address from label.
-	adrp x9, str0@PAGE
-	add x9, x9, str0@PAGEOFF
-
-	//print string.
-	mov x0, x9
-	bl _printf
 
 	//move number to register.
 	mov x9, #1
@@ -78,13 +105,7 @@ _main:
 	mov x1, x9
 
 	//call function.
-	bl _add
-
-	//pass parameter.
-	mov x0, x0
-
-	//call function.
-	bl _double
+	bl _compose
 
 	//ensure int to print doesn't get clobbered.
 	mov x9, x0
