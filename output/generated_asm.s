49e8cd06a2e7ef3300827db08439ec2c1df9fcfe
christiantomato <christianntamayo@gmail.com>
2026-05-17 09:38:56 -0400

    works!!!

--- output/generated_asm.s
+++ output/generated_asm.s
@@ -1,14 +1,15 @@
 .data
 fmt_int: .asciz "%d\n"
+str0: .asciz "hi :3\n"
 
 .text
 .global _main
 
-_add:
+_complex:
 	//setup stack frame.
 	stp fp, lr, [sp, #-16]!
 	mov fp, sp
-	sub sp, sp, #32
+	sub sp, sp, #48
 
 	//spill param to stack.
 	str x0, [fp, #-8]
@@ -16,20 +17,44 @@ _add:
 	//spill param to stack.
 	str x1, [fp, #-16]
 
+	//spill param to stack.
+	str x2, [fp, #-24]
+
+	//spill param to stack.
+	str x3, [fp, #-32]
+
 	//load variable from stack.
 	ldr x9, [fp, #-8]
 
 	//load variable from stack.
-	ldr x10, [fp, #-16]
+	ldr x10, [fp, #-32]
+
+	//load variable from stack.
+	ldr x11, [fp, #-16]
 
 	//binary operation.
-	add x11, x9, x10
+	mul x12, x10, x11
 
-	//store variable to stack.
-	str x11, [fp, #-24]
+	//binary operation.
+	add x10, x9, x12
 
 	//load variable from stack.
 	ldr x9, [fp, #-24]
+
+	//binary operation.
+	add x11, x10, x9
+
+	//move number to register.
+	mov x9, #1
+
+	//binary operation.
+	sub x10, x11, x9
+
+	//store variable to stack.
+	str x10, [fp, #-40]
+
+	//load variable from stack.
+	ldr x9, [fp, #-40]
 
 	//return into x0.
 	mov x0, x9
@@ -39,11 +64,11 @@ _add:
 	ldp fp, lr, [sp], #16
 	ret
 
-_compose:
+_composition:
 	//setup stack frame.
 	stp fp, lr, [sp, #-16]!
 	mov fp, sp
-	sub sp, sp, #32
+	sub sp, sp, #48
 
 	//spill param to stack.
 	str x0, [fp, #-8]
@@ -51,35 +76,57 @@ _compose:
 	//spill param to stack.
 	str x1, [fp, #-16]
 
+	//spill param to stack.
+	str x2, [fp, #-24]
+
+	//spill param to stack.
+	str x3, [fp, #-32]
+
 	//load variable from stack.
 	ldr x9, [fp, #-8]
 
-	//pass parameter.
+	//print string.
 	mov x0, x9
+	bl _printf
 
 	//load variable from stack.
 	ldr x9, [fp, #-16]
 
 	//pass parameter.
-	mov x1, x9
-
-	//call function.
-	bl _add
-
-	//move number to register.
-	mov x9, #1
-
-	//binary operation.
-	add x10, x0, x9
-
-	//store variable to stack.
-	str x10, [fp, #-24]
+	mov x0, x9
 
 	//load variable from stack.
 	ldr x9, [fp, #-24]
 
+	//pass parameter.
+	mov x1, x9
+
+	//load variable from stack.
+	ldr x9, [fp, #-32]
+
+	//pass parameter.
+	mov x2, x9
+
+	//move number to register.
+	mov x9, #10
+
+	//pass parameter.
+	mov x3, x9
+
+	//call function.
+	bl _complex
+
+	//store variable to stack.
+	str x0, [fp, #-40]
+
+	//load variable from stack.
+	ldr x9, [fp, #-40]
+
+	//negate value.
+	neg x10, x9
+
 	//return into x0.
-	mov x0, x9
+	mov x0, x10
 
 	//collapse the stack frame.
 	mov sp, fp
@@ -90,29 +137,48 @@ _main:
 	//setup stack frame.
 	stp fp, lr, [sp, #-16]!
 	mov fp, sp
-	sub sp, sp, #0
+	sub sp, sp, #16
 
-	//move number to register.
-	mov x9, #1
-
-	//pass parameter.
-	mov x0, x9
-
-	//move number to register.
-	mov x9, #2
+	//load string address from label.
+	adrp x10, str0@PAGE
+	add x10, x10, str0@PAGEOFF
 
 	//pass parameter.
-	mov x1, x9
+	mov x0, x10
+
+	//move number to register.
+	mov x10, #1
+
+	//pass parameter.
+	mov x1, x10
+
+	//move number to register.
+	mov x10, #2
+
+	//pass parameter.
+	mov x2, x10
+
+	//move number to register.
+	mov x10, #3
+
+	//pass parameter.
+	mov x3, x10
 
 	//call function.
-	bl _compose
+	bl _composition
+
+	//store variable to stack.
+	str x0, [fp, #-8]
+
+	//load variable from stack.
+	ldr x10, [fp, #-8]
 
 	//ensure int to print doesn't get clobbered.
-	mov x9, x0
+	mov x11, x10
 	//print integer.
 	adrp x0, fmt_int@PAGE
 	add x0, x0, fmt_int@PAGEOFF
-	str x9, [sp, #-16]!
+	str x11, [sp, #-16]!
 	bl _printf
 	add sp, sp, #16
 
