0f2252839cda5e3e23122620f1bbf4366596d823
christiantomato <christianntamayo@gmail.com>
2026-05-20 13:07:36 -0400

    test seems to work

--- output/generated_asm.s
+++ output/generated_asm.s
@@ -1,59 +1,29 @@
 .data
 fmt_int: .asciz "%d\n"
-str0: .asciz "hiiiiii\n"
+str0: .asciz "hi mom\n"
+str1: .asciz "hi dad\n"
+str2: .asciz "cry\n"
 
 .text
 .global _main
-
-_voidFN:
-	//setup stack frame.
-	stp fp, lr, [sp, #-16]!
-	mov fp, sp
-	sub sp, sp, #16
-
-	//spill param to stack.
-	str x0, [fp, #-8]
-
-	//load variable from stack.
-	ldr x9, [fp, #-8]
-
-	//print string.
-	mov x0, x9
-	bl _printf
-
-	//load variable from stack.
-	ldr x9, [fp, #-8]
-
-	//print string.
-	mov x0, x9
-	bl _printf
-
-	//move number to register.
-	mov x9, #0
-
-	//return into x0.
-	mov x0, x9
-
-	//collapse the stack frame.
-	mov sp, fp
-	ldp fp, lr, [sp], #16
-	ret
 
 _main:
 	//setup stack frame.
 	stp fp, lr, [sp, #-16]!
 	mov fp, sp
-	sub sp, sp, #0
+	sub sp, sp, #16
 
-	//load string address from label.
-	adrp x9, str0@PAGE
-	add x9, x9, str0@PAGEOFF
+	//move number to register.
+	mov x9, #1
 
-	//pass parameter.
-	mov x0, x9
+	//store variable to stack.
+	str x9, [fp, #-8]
 
-	//call function.
-	bl _voidFN
+	//move number to register.
+	mov x9, #0
+
+	//store variable to stack.
+	str x9, [fp, #-16]
 
 	//put status code 0 in ret register.
 	mov x0, #0
