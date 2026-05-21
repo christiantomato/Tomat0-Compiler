b5f80d89c4d40d3aa0189c80964bdca0f007fbc8
christiantomato <christianntamayo@gmail.com>
2026-05-20 23:48:41 -0400

    recursive factorial tomat0 implementation.

--- output/generated_asm.s
+++ output/generated_asm.s
@@ -8,7 +8,7 @@ _recursiveFactorial:
 	//setup stack frame.
 	stp fp, lr, [sp, #-16]!
 	mov fp, sp
-	sub sp, sp, #32
+	sub sp, sp, #16
 
 	//spill param to stack.
 	str x0, [fp, #-8]
@@ -50,38 +50,26 @@ _endif0:
 	//binary operation.
 	sub x12, x9, x10
 
-	//store variable to stack.
-	str x12, [fp, #-16]
-
-	//load variable from stack.
-	ldr x9, [fp, #-16]
-
 	//pass parameter.
-	mov x0, x9
+	mov x0, x12
 
 	//call function.
 	bl _recursiveFactorial
 
 	//store variable to stack.
-	str x0, [fp, #-24]
+	str x0, [fp, #-16]
 
 	//load variable from stack.
 	ldr x9, [fp, #-8]
 
 	//load variable from stack.
-	ldr x10, [fp, #-24]
+	ldr x10, [fp, #-16]
 
 	//binary operation.
 	mul x12, x9, x10
 
-	//store variable to stack.
-	str x12, [fp, #-8]
-
-	//load variable from stack.
-	ldr x9, [fp, #-8]
-
 	//return into x0.
-	mov x0, x9
+	mov x0, x12
 
 	//collapse the stack frame.
 	mov sp, fp
@@ -92,7 +80,7 @@ _recursiveFactorialBad:
 	//setup stack frame.
 	stp fp, lr, [sp, #-16]!
 	mov fp, sp
-	sub sp, sp, #32
+	sub sp, sp, #16
 
 	//spill param to stack.
 	str x0, [fp, #-8]
@@ -128,44 +116,26 @@ _endif1:
 	//load variable from stack.
 	ldr x9, [fp, #-8]
 
+	//load variable from stack.
+	ldr x10, [fp, #-8]
+
 	//move number to register.
-	mov x10, #1
+	mov x13, #1
 
 	//binary operation.
-	sub x13, x9, x10
-
-	//store variable to stack.
-	str x13, [fp, #-16]
-
-	//load variable from stack.
-	ldr x9, [fp, #-16]
+	sub x14, x10, x13
 
 	//pass parameter.
-	mov x0, x9
+	mov x0, x14
 
 	//call function.
-	bl _recursiveFactorial
-
-	//store variable to stack.
-	str x0, [fp, #-24]
-
-	//load variable from stack.
-	ldr x9, [fp, #-8]
-
-	//load variable from stack.
-	ldr x10, [fp, #-24]
+	bl _recursiveFactorialBad
 
 	//binary operation.
-	mul x13, x9, x10
-
-	//store variable to stack.
-	str x13, [fp, #-8]
-
-	//load variable from stack.
-	ldr x9, [fp, #-8]
+	mul x10, x9, x0
 
 	//return into x0.
-	mov x0, x9
+	mov x0, x10
 
 	//collapse the stack frame.
 	mov sp, fp
