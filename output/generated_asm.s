53cebfa13273ed9bf9c71d58b9c761ec91c5becc
christiantomato <christianntamayo@gmail.com>
2026-05-21 11:44:54 -0400

    thrown tomato example

--- output/generated_asm.s
+++ output/generated_asm.s
@@ -1,146 +1,10 @@
 .data
 fmt_int: .asciz "%d\n"
+str0: .asciz "aaahhh\n"
+str1: .asciz "were okay don't worry\n"
 
 .text
 .global _main
-
-_recursiveFactorial:
-	//setup stack frame.
-	stp fp, lr, [sp, #-16]!
-	mov fp, sp
-	sub sp, sp, #16
-
-	//spill param to stack.
-	str x0, [fp, #-8]
-
-	//load variable from stack.
-	ldr x9, [fp, #-8]
-
-	//move number to register.
-	mov x10, #1
-
-	//binary operation.
-	cmp x9, x10
-	cset x11, eq
-
-	//check condition.
-	cmp x11, #1
-	//if not.
-	bne _endif0
-
-_if0:
-	//load variable from stack.
-	ldr x9, [fp, #-8]
-
-	//return into x0.
-	mov x0, x9
-
-	//collapse the stack frame.
-	mov sp, fp
-	ldp fp, lr, [sp], #16
-	ret
-
-_endif0:
-	//load variable from stack.
-	ldr x9, [fp, #-8]
-
-	//move number to register.
-	mov x10, #1
-
-	//binary operation.
-	sub x12, x9, x10
-
-	//pass parameter.
-	mov x0, x12
-
-	//call function.
-	bl _recursiveFactorial
-
-	//store variable to stack.
-	str x0, [fp, #-16]
-
-	//load variable from stack.
-	ldr x9, [fp, #-8]
-
-	//load variable from stack.
-	ldr x10, [fp, #-16]
-
-	//binary operation.
-	mul x12, x9, x10
-
-	//return into x0.
-	mov x0, x12
-
-	//collapse the stack frame.
-	mov sp, fp
-	ldp fp, lr, [sp], #16
-	ret
-
-_recursiveFactorialBad:
-	//setup stack frame.
-	stp fp, lr, [sp, #-16]!
-	mov fp, sp
-	sub sp, sp, #16
-
-	//spill param to stack.
-	str x0, [fp, #-8]
-
-	//load variable from stack.
-	ldr x9, [fp, #-8]
-
-	//move number to register.
-	mov x10, #1
-
-	//binary operation.
-	cmp x9, x10
-	cset x12, eq
-
-	//check condition.
-	cmp x12, #1
-	//if not.
-	bne _endif1
-
-_if1:
-	//load variable from stack.
-	ldr x9, [fp, #-8]
-
-	//return into x0.
-	mov x0, x9
-
-	//collapse the stack frame.
-	mov sp, fp
-	ldp fp, lr, [sp], #16
-	ret
-
-_endif1:
-	//load variable from stack.
-	ldr x9, [fp, #-8]
-
-	//load variable from stack.
-	ldr x10, [fp, #-8]
-
-	//move number to register.
-	mov x13, #1
-
-	//binary operation.
-	sub x14, x10, x13
-
-	//pass parameter.
-	mov x0, x14
-
-	//call function.
-	bl _recursiveFactorialBad
-
-	//binary operation.
-	mul x10, x9, x0
-
-	//return into x0.
-	mov x0, x10
-
-	//collapse the stack frame.
-	mov sp, fp
-	ldp fp, lr, [sp], #16
-	ret
 
 _main:
 	//setup stack frame.
@@ -157,20 +21,40 @@ _main:
 	//load variable from stack.
 	ldr x9, [fp, #-8]
 
-	//pass parameter.
+	//move number to register.
+	mov x10, #0
+
+	//binary operation.
+	cmp x9, x10
+	cset x11, gt
+
+	//check condition.
+	cmp x11, #1
+	//if not.
+	bne _endif0
+
+_if0:
+	//load string address from label.
+	adrp x9, str0@PAGE
+	add x9, x9, str0@PAGEOFF
+
+	//print string.
 	mov x0, x9
-
-	//call function.
-	bl _recursiveFactorial
-
-	//ensure int to print doesn't get clobbered.
-	mov x9, x0
-	//print integer.
-	adrp x0, fmt_int@PAGE
-	add x0, x0, fmt_int@PAGEOFF
-	str x9, [sp, #-16]!
 	bl _printf
-	add sp, sp, #16
+
+	//throw tomato - terminate runtime.
+	mov x0, #1
+	mov x16, #1
+	svc #0x80
+
+_endif0:
+	//load string address from label.
+	adrp x9, str1@PAGE
+	add x9, x9, str1@PAGEOFF
+
+	//print string.
+	mov x0, x9
+	bl _printf
 
 	//put status code 0 in ret register.
 	mov x0, #0
